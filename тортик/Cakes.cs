24736e98269ebc2bd308812ce4ac06dac9835976
grecemam <isip_m.s.maksimenkova@mpt.ru>
2022-10-28 22:45:02 +0300

    ура победа

---
+++ тортик/Cakes.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace тортик
+{
+    internal class Cakes
+    {
+        public string osn;
+        public List<DopCakes> dops;
+
+    }
+}
---
+++ тортик/DopCakes.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace тортик
+{
+    internal class DopCakes
+    {
+        public DopCakes(string Dop, int Price)
+        {
+            dop = Dop;
+            price = Price;
+        }
+        public int price;
+        public string dop;
+    }
+}
